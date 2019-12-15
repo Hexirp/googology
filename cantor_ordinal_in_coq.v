1bf5756cad1dfbfa566fe922718741fd8ae1d883
Hexirp Hexirp@users.noreply.github.com <Hexirp@users.noreply.github.com>
2019-12-15 11:00:13 +0900

    Update: cantor_ordinal_in_coq

--- cantor_ordinal_in_coq.v
+++ cantor_ordinal_in_coq.v
@@ -10,7 +10,7 @@ Require Import Coq.Init.Prelude.
 (* たとえば ε_0 は Cantor ordinal と巨大数界隈では呼ばれることがある。 *)
 (* カントール標準形 (Cantor normal form) の限界であるからだ。 *)
 (* この名前は ε_0 という名前より幾ばくか味が付いているように感じる。 *)
-(* なので、この記事では epsilon_zero とかではなく cantor_ord とかを使う。 *)
+(* なので、この記事では epsilon_zero とかではなく cantor_ordinal とかを使う。 *)
 
 (* ε_0 の整礎性を証明するための方針はこうだ。 *)
 
@@ -24,4 +24,22 @@ Require Import Coq.Init.Prelude.
 Definition nat_iter {P : Type} (o : P) (s : P -> P) (n : nat) : P
   := @nat_rect (fun _ => P) o (fun _ => s) n.
 
-Definition cantor_ord : Type := { n & nat_iter Empty_set list n }.
+Definition cantor_ordinal_term : Type := { n & nat_iter Empty_set list n }.
+
+(* 順序数表記の順序を定義する。 *)
+
+Definition relation (A : Type) : Type := A -> A -> Type.
+
+Definition empty_order : relation Empty_set
+  := @Empty_set_rect (fun _ => Empty_set -> Type).
+
+Open Scope list_scope.
+
+Definition lexicographical_order (A : Type) (order_A : relation A) : relation (list A)
+  := fix order (x : list A) (y : list A) : Type
+    := match x, y with
+      | nil, nil => Empty_set
+      | nil, yv :: ys => unit
+      | xv :: xs, nil => Empty_set
+      | xv :: xs, yv :: ys => sum (order_A xv yv) (order xs ys)
+    end.
