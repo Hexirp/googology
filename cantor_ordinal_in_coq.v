db8b104e046ded885b479d63765459e61ae1359c
Hexirp Hexirp@users.noreply.github.com <Hexirp@users.noreply.github.com>
2019-12-15 15:36:19 +0900

    Update: cantor_ordinal

--- cantor_ordinal_in_coq.v
+++ cantor_ordinal_in_coq.v
@@ -21,6 +21,17 @@ Open Scope list_scope.
 
 (* それでは始めよう。 *)
 
+Definition eq_rect_nop
+  {A : Type} (P : A -> A -> Type)
+  (f : forall a : A, P a a)
+  (x y : A) (e : x = y) : P x y.
+Proof.
+  elim e.
+  exact (f x).
+Defined.
+
+Definition order (A : Type) : Type := A -> A -> comparison.
+
 (* 順序数表記の項を定義する。 *)
 
 Definition nat_iter {P : Type} (o : P) (s : P -> P) (n : nat) : P
@@ -29,8 +40,6 @@ Definition nat_iter {P : Type} (o : P) (s : P -> P) (n : nat) : P
 Definition cantor_ordinal_term : Type := { n & nat_iter Empty_set list n }.
 
 (* 順序数表記の順序を定義する。 *)
-
-Definition order (A : Type) : Type := A -> A -> comparison.
 
 Definition empty_order : order Empty_set
   := @Empty_set_rect (fun _ => Empty_set -> comparison).
@@ -48,6 +57,38 @@ Definition lexicographical_order {A : Type} (order_A : order A) : order (list A)
       end
     end.
 
+Definition nat_order : order nat
+  := fix f (x : nat) (y : nat) : comparison
+    := match x, y with
+      | O, O => Eq
+      | O, S yp => Lt
+      | S xp, O => Gt
+      | S xp, S yp => f xp yp
+    end.
+
+Definition eq_reflection_nat_order : forall x y, nat_order x y = Eq -> x = y.
+Proof.
+  refine (fix f (x : nat) (y : nat) {struct x} : nat_order x y = Eq -> x = y := _).
+  refine (match x, y with
+    | O, O => _
+    | O, S yp => _
+    | S xp, O => _
+    | S xp, S yp => _
+  end).
+  - refine (fun _ => eq_refl).
+  - refine (fun p => _).
+    change (Lt = Eq) in p.
+    discriminate.
+  - refine (fun p => _).
+    change (Gt = Eq) in p.
+    discriminate.
+  - refine (fun p => _).
+    change (nat_order xp yp = Eq) in p.
+    apply eq_S.
+    apply f.
+    exact p.
+Defined.
+
 Fixpoint cantor_nat_iter_order (n : nat) : order (nat_iter Empty_set list n)
   := match n with
     | O => empty_order
@@ -56,9 +97,18 @@ Fixpoint cantor_nat_iter_order (n : nat) : order (nat_iter Empty_set list n)
 
 Definition cantor_ordinal_order : order cantor_ordinal_term
   := fun x y => match x, y with
-    | existT _ xn xe, existT _ yn ye => match nat_order xn yn with
-      | Eq => cantor_nat_iter_order xyn xe ye
-      | Lt => Lt
-      | Gt => Gt
-      end
+    | existT _ xn xe, existT _ yn ye => (match nat_order xn yn as c return nat_order xn yn = c -> comparison with
+      | Eq => fun p =>
+  eq_rect_nop
+    (fun x y => nat_iter Empty_set list x -> nat_iter Empty_set list y -> comparison)
+    (fun z xs ys => cantor_nat_iter_order z xs ys)
+    xn
+    yn
+    (eq_reflection_nat_order xn yn p)
+    xe
+    ye
+      | Lt => fun _ => Lt
+      | Gt => fun _ => Gt
+      end)
+  eq_refl
     end.
